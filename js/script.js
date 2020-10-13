8173e556624c968688e8a71db182f0b935dae436
dbullard2 <daniel.earl.bullard@gmail.com>
2020-10-12 19:21:13 -0500

    all done!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-document.getElementById('form').reset();
+var page = document.body.id;
+var home = document.getElementsByClassName('home')[0];
+var menu = document.getElementsByClassName('menu-nav')[0];
+var about = document.getElementsByClassName('about')[0];
+var contact = document.getElementsByClassName('contact')[0];
+
+//document.getElementById('form').reset();
+
+if (page == 'home') {
+  home.classList.add('active');
+} else if (page == 'menu') {
+  menu.classList.add('active');
+} else if (page == 'about') {
+  about.classList.add('active');
+} else if (page == 'contact') {
+  contact.classList.add('active');
+}
